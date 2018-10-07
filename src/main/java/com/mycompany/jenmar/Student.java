d8b8d170acf289e98c768d59f0c8ecc8b51d0c0e
Kelvin Ashu <kelvin@pinevalleyinstitute.co.za>
2018-10-07 14:31:18 +0200

    initial commit

---
+++ src/main/java/com/mycompany/jenmar/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jenmar;
+
+/**
+ *
+ * @author kelvinashu
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        for (int k = 0; k < 10; k++) {
+            System.out.println("Mar value " + k);
+        }
+    }
+
+}
